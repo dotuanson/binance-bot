1cda60d6f2bbfe9ac96755766865897d9f74139c
dotuanson <son.do@emandai.net>
2023-10-27 00:00:27 +0700

    feat: load env using viper package

---
+++ util/config.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+)
+
+// Config stores all configuration of the application.
+// The values are read by viper from a config file or environment variable.
+type Config struct {
+	ApiKEY     string `mapstructure:"API_KEY"`
+	SecretKEY  string `mapstructure:"SECRET_KEY"`
+	BaseURL    string `mapstructure:"BASE_URL"`
+	TeleTOKEN  string `mapstructure:"TELEGRAM_TOKEN"`
+	TeleCHATID int64
+}
+
+// LoadConfig reads configuration from file or environment variables.
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("dev")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	config.TeleCHATID = viper.GetInt64("TELEGRAM_CHATID")
+	return
+}
