8d40de75e7d920cfbe9dcc42fb0dfb772559a6ea
dotuanson <dotuansondn@gmail.com>
2023-10-28 09:03:24 +0700

    feat: return time in UTC+7

--- internal/rss/aggregator.go
+++ internal/rss/aggregator.go
@@ -18,8 +18,10 @@ func FeedRSS(feedUrl string, textCh chan<- string) {
 		go func(item *gofeed.Item) {
 			duration := currTime.Sub(*item.PublishedParsed)
 			if duration < 12*time.Hour {
-				text := fmt.Sprintf(`[%v] - Title: %v
-					Link: %v`, *item.PublishedParsed, item.Title, item.Link)
+				location, _ := time.LoadLocation("Asia/Bangkok")
+				currentTimeUTC7 := item.PublishedParsed.In(location)
+				currentTimeUTC7Format := currentTimeUTC7.Format("2006/01/02 - 15:04")
+				text := fmt.Sprintf("[%v] %v\nLink: %v", currentTimeUTC7Format, item.Title, item.Link)
 				log.Print(text)
 				textCh <- text
 			}
