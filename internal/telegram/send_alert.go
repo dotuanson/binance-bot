ead3791fcbf7e5b5c7139f195440aea125ea4450
dotuanson <dotuansondn@gmail.com>
2023-10-28 09:03:09 +0700

    fix: remove preview alert in telegram

--- internal/telegram/send_alert.go
+++ internal/telegram/send_alert.go
@@ -4,8 +4,9 @@ import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
 func SendTeleAlert(bot *tgbotapi.BotAPI, teleChatID int64, textCh <-chan string, errCh chan<- error) {
 	for {
-		text := tgbotapi.NewMessage(teleChatID, <-textCh)
-		_, err := bot.Send(text)
+		msg := tgbotapi.NewMessage(teleChatID, <-textCh)
+		msg.DisableWebPagePreview = true
+		_, err := bot.Send(msg)
 		if err != nil {
 			errCh <- err
 		}
