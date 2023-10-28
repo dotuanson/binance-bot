25140c20819f5a409d89839a006ca14e4af2249f
dotuanson <dotuansondn@gmail.com>
2023-10-28 08:48:24 +0700

    fix: add sleep to test rss to fix blocking pipeline

--- test/rss_test.go
+++ test/rss_test.go
@@ -3,9 +3,11 @@ package test
 import (
 	"github.com/dotuanson/binance-bot/internal/rss"
 	"testing"
+	"time"
 )
 
 func TestRSS(t *testing.T) {
 	textCh := make(chan string, 10)
-	rss.FeedRSS("https://cointelegraph.com/rss/tag/bitcoin", textCh)
+	go rss.FeedRSS("https://cointelegraph.com/rss/tag/bitcoin", textCh)
+	time.Sleep(time.Second * 10)
 }
