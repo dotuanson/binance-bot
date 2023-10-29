5d04e61d1ee35309e910d2b1d35388260491745a
dotuanson <dotuansondn@gmail.com>
2023-10-29 21:13:53 +0700

    feat: increase threshold percent to mitigate spaming alert

--- internal/binance/avg_price_watcher.go
+++ internal/binance/avg_price_watcher.go
@@ -14,7 +14,12 @@ const (
 )
 
 func WatchAvgPrice(ctx context.Context, client *binanceConnector.Client, textCh chan<- string, errCh chan<- error, coin string) {
+	var (
+		thresholdUp   float64 = 1.0
+		thresholdDown float64 = 1.0
+	)
 	for {
+
 		kLines, err := client.NewKlinesService().Symbol(coin + unitPrice).Interval("5m").Do(ctx)
 		if err != nil {
 			errCh <- err
@@ -32,16 +37,31 @@ func WatchAvgPrice(ctx context.Context, client *binanceConnector.Client, textCh 
 		percent := diff / openPrice * 100
 		log.Printf("Coin: %s, "+
 			"Percent: %.2f %%, "+
-			"Close Price %f, "+
-			"Open Price %f", coin, percent, closePrice, openPrice)
-		if (percent - 1) >= 0 {
-			textCh <- fmt.Sprintf("🚀 [%s] is BULLISH (up 1%%) in 5m, "+
-				"price: %f USDT\n", coin, closePrice)
-			time.Sleep(time.Second * 15)
-		} else if (-percent - 1) >= 0 {
-			textCh <- fmt.Sprintf("🔥 [%s] is BEARISH (down 1%%) in 5m, "+
-				"price: %f USDT\n", coin, closePrice)
-			time.Sleep(time.Second * 15)
+			"Close Price: %f, "+
+			"Open Price: %f, "+
+			"Threshold Up: %f, "+
+			"Threshold Down: %f", coin, percent, closePrice, openPrice, thresholdUp, thresholdDown)
+		if (percent - thresholdUp) >= 0 {
+			textCh <- fmt.Sprintf("🚀 [%s] is BULLISH (up %.2f%%) in 5m, "+
+				"price: %f USDT\n", coin, thresholdUp, closePrice)
+			thresholdUp += 0.1
+			time.Sleep(time.Second * 25)
+		} else if (-percent - thresholdDown) >= 0 {
+			textCh <- fmt.Sprintf("🔥 [%s] is BEARISH (down %.2f%%) in 5m, "+
+				"price: %f USDT\n", coin, thresholdDown, closePrice)
+			thresholdDown += 0.1
+			time.Sleep(time.Second * 25)
+		} else {
+			if thresholdUp -= 0.1; (thresholdUp - 1.0) <= 0.0 {
+				thresholdUp = 1.0
+			} else {
+				time.Sleep(time.Second * 25)
+			}
+			if thresholdDown -= 0.1; (thresholdDown - 1.0) <= 0.0 {
+				thresholdDown = 1.0
+			} else {
+				time.Sleep(time.Second * 25)
+			}
 		}
 		time.Sleep(time.Second * 5)
 	}
