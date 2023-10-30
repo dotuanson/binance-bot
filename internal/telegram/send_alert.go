b78383200813a33aee889709726288eb8b015537
dotuanson <dotuansondn@gmail.com>
2023-10-30 13:56:18 +0700

    feat: add parse mode markdown for telegram alerter

--- internal/telegram/send_alert.go
+++ internal/telegram/send_alert.go
@@ -6,6 +6,7 @@ func SendTeleAlert(bot *tgbotapi.BotAPI, teleChatID int64, textCh <-chan string,
 	for {
 		msg := tgbotapi.NewMessage(teleChatID, <-textCh)
 		msg.DisableWebPagePreview = true
+		msg.ParseMode = "markdown"
 		_, err := bot.Send(msg)
 		if err != nil {
 			errCh <- err
