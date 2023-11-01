192eb892c76e30777194df8dd44bc17b0f1241d5
dotuanson <dotuansondn@gmail.com>
2023-11-01 10:41:26 +0700

    feat: add whale watcher

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	binanceConnector "github.com/binance/binance-connector-go"
 	"github.com/dotuanson/binance-bot/internal/binance"
-	"github.com/dotuanson/binance-bot/internal/rss"
 	"github.com/dotuanson/binance-bot/internal/telegram"
 	"github.com/dotuanson/binance-bot/util"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -33,15 +32,19 @@ func main() {
 
 	client := binanceConnector.NewClient(config.ApiKEY, config.SecretKEY, config.BaseURL)
 	textCh := make(chan string, 10)
+	//for _, coin := range config.CoinLIST {
+	//	if coin == "BTC" {
+	//		go binance.WatchAvgPrice(ctx, client, 0.5, textCh, errCh, coin)
+	//
+	//	} else {
+	//		go binance.WatchAvgPrice(ctx, client, 1.5, textCh, errCh, coin)
+	//	}
+	//}
 	for _, coin := range config.CoinLIST {
-		if coin == "BTC" {
-			go binance.WatchAvgPrice(ctx, client, 0.5, textCh, errCh, coin)
-		} else {
-			go binance.WatchAvgPrice(ctx, client, 1.5, textCh, errCh, coin)
-		}
+		go binance.WhaleWatcher(ctx, client, textCh, errCh, coin)
 	}
 	go telegram.SendTeleAlert(bot, config.TeleCHATID, textCh, errCh)
-	go rss.FeedRSS(FeedUrls, textCh)
+	//go rss.FeedRSS(FeedUrls, textCh)
 	for {
 		err = <-errCh
 		log.Fatal(err)
---
+++ internal/binance/whale_watcher.go
@@ -0,0 +1,83 @@
+package binance
+
+import (
+	"context"
+	"fmt"
+	binanceConnector "github.com/binance/binance-connector-go"
+	"log"
+	"strconv"
+	"time"
+)
+
+func WhaleWatcher(ctx context.Context, client *binanceConnector.Client, textCh chan<- string, errCh chan<- error, coin string) {
+	const (
+		ratioVolume    = 10.0
+		numberOfKlines = 3
+	)
+	for {
+		kLines, err := client.NewKlinesService().Symbol(coin + unitPrice).
+			Interval("1m").Limit(numberOfKlines).
+			Do(ctx)
+		if err != nil {
+			errCh <- err
+		}
+		if len(kLines) != numberOfKlines {
+			continue
+		}
+		lastPrice, err := strconv.ParseFloat(kLines[numberOfKlines-1].Close, 64)
+		if err != nil {
+			errCh <- err
+		}
+		lastVolume, err := strconv.ParseFloat(kLines[numberOfKlines-1].Volume, 64)
+		if err != nil {
+			errCh <- err
+		}
+		middlePrice, err := strconv.ParseFloat(kLines[numberOfKlines-2].Close, 64)
+		if err != nil {
+			errCh <- err
+		}
+		middleVolume, err := strconv.ParseFloat(kLines[numberOfKlines-2].Volume, 64)
+		if err != nil {
+			errCh <- err
+		}
+		firstPrice, err := strconv.ParseFloat(kLines[numberOfKlines-1].Close, 64)
+		if err != nil {
+			errCh <- err
+		}
+		firstVolume, err := strconv.ParseFloat(kLines[numberOfKlines-3].Volume, 64)
+		if err != nil {
+			errCh <- err
+		}
+		if lastVolume < 1e-9 {
+			continue
+		}
+		log.Printf("Coin: %s, "+
+			"Current Price: %.2f, "+
+			"Last/ Middle Volume: %f, "+
+			"Last/ First Volume: %f", coin, lastPrice, lastVolume/middleVolume, lastVolume/firstVolume)
+		if lastVolume > 2*ratioVolume*firstVolume {
+			diff := lastPrice - firstPrice
+			percent := diff / firstPrice * 100
+			if percent >= 0 {
+				textCh <- fmt.Sprintf("[x200] 🚀 %s is having a bull-run with %.2f%%,"+
+					"current price: %f USDT", coin, percent, lastPrice)
+			} else {
+				textCh <- fmt.Sprintf("[x200] 🔥 %s is having a bull-run with %.2f%%,"+
+					"current price: %f USDT", coin, percent, lastPrice)
+			}
+			time.Sleep(time.Second * 30)
+		} else if lastVolume > ratioVolume*middleVolume {
+			diff := lastPrice - middlePrice
+			percent := diff / middlePrice * 100
+			if percent >= 0 {
+				textCh <- fmt.Sprintf("[x100] 🚀 %s has just modified %.2f%%,"+
+					"current price: %f USDT", coin, percent, lastPrice)
+			} else {
+				textCh <- fmt.Sprintf("[x100] 🔥 %s has just modified %.2f%%,"+
+					"current price: %f USDT", coin, percent, lastPrice)
+			}
+			time.Sleep(time.Second * 20)
+		}
+		time.Sleep(time.Second * 1)
+	}
+}
